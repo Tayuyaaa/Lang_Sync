69b6eab6a66d67b79f51899a457e341ff4c22328
Tayuya <131668149+Tayuyaaa@users.noreply.github.com>
2026-03-24 00:47:47 +0100

    Update background.js
    Таймер-запобіжник (Debounce): Скрипт має почекати мікросекунду (наприклад, 100 мілісекунд), щоб переконатися, що ти зупинився на цій вкладці, і тільки тоді смикати Вінду.
    
    Слухач вікон: Скрипт має реагувати не тільки на вкладки, а й на зміну фокусу між самими вікнами браузера.

--- MyLangExt/background.js
+++ MyLangExt/background.js
@@ -1,6 +1,7 @@
 const hostName = "com.rybka.lang.switcher";
+let debounceTimer = null;
 
-// Зберігаємо мову в постійну пам'ять Хрому (замість змінної)
+// Зберігаємо мову в постійну пам'ять Хрому
 chrome.runtime.onMessage.addListener((msg, sender) => {
     if (msg.lang && sender.tab) {
         let key = 'tab_' + sender.tab.id;
@@ -8,18 +9,39 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
     }
 });
 
-// Перемикаємо мову, витягуючи дані з бази
+// Розумна функція перемикання із ЗАПОБІЖНИКОМ
+function switchLanguage(tabId) {
+    clearTimeout(debounceTimer); // Вбиваємо попередній наказ, якщо ти швидко клацнув далі
+    
+    // Чекаємо 100 мілісекунд, щоб Вінда точно зафіксувала нове вікно як активне
+    debounceTimer = setTimeout(() => {
+        let key = 'tab_' + tabId;
+        chrome.storage.local.get([key], (result) => {
+            let lang = result[key];
+            if (lang) {
+                chrome.runtime.sendNativeMessage(hostName, { text: lang });
+            }
+        });
+    }, 100); 
+}
+
+// Слухаємо перемикання ВКЛАДОК
 chrome.tabs.onActivated.addListener(activeInfo => {
-    let key = 'tab_' + activeInfo.tabId;
-    chrome.storage.local.get([key], (result) => {
-        let lang = result[key];
-        if (lang) {
-            chrome.runtime.sendNativeMessage(hostName, { text: lang });
-        }
-    });
+    switchLanguage(activeInfo.tabId);
 });
 
-// Очищаємо базу від сміття, коли вкладка закривається
+// Слухаємо перемикання ВІКОН Хрому
+chrome.windows.onFocusChanged.addListener(windowId => {
+    if (windowId !== chrome.windows.WINDOW_ID_NONE) {
+        chrome.tabs.query({ active: true, windowId: windowId }, tabs => {
+            if (tabs.length > 0) {
+                switchLanguage(tabs[0].id);
+            }
+        });
+    }
+});
+
+// Очищаємо базу від сміття при закритті вкладки
 chrome.tabs.onRemoved.addListener(tabId => {
     let key = 'tab_' + tabId;
     chrome.storage.local.remove(key);
